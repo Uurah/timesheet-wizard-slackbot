9c096582cc3d6908e67d6bb0344697b8c685b5e5
Eso-vendition <nic.mann@esolutionsone.com>
2017-08-21 13:09:54 -0700

    stringify actionJSON parsed

--- routes/routes.js
+++ routes/routes.js
@@ -10,59 +10,9 @@ var urlencodedParser = bodyParser.urlencoded({extended: false})
 var appRouter = function (app) {
     var request = require('request');
 
-    var crypto;
-    var algorithm = 'aes-256-ctr';
-    var password = 'd6F3Efeq';
-    try {
-        crypto = require('crypto');
-    } catch (err) {
-        console.log('crypto support is disabled!');
-    }
-
-    var sessionStore = {};
-    var sessionID = 0;
-
     var instanceURL = 'https://esolutionsgroupdemo2.service-now.com';
     var apiURI = '/api/x_esg_vendition_e/slack';
     var encoded = new Buffer('api:apit3st!').toString('base64');
-
-    function encrypt(text, res, body) {
-        var cipher = crypto.createCipher(algorithm, password);
-        var crypted = cipher.update(text, 'utf8', 'hex');
-        crypted += cipher.final('hex');
-
-        function storeSession(key, text) {
-            body = [sessionID, key];
-            sessionStore[sessionID] = {"key": key, "text": text, "timeout": null};
-            console.log("storeSession ID: " + sessionID);
-            console.log("storeSession KEY: " + sessionStore[sessionID].key);
-            console.log("storeSession TEXT: " + sessionStore[sessionID].text);
-            sessionID += 1;
-            return res.status(200).send(body);
-        }
-
-        storeSession(crypted, text);
-    }
-
-    function decrypt(text) {
-        var decipher = crypto.createDecipher(algorithm, password);
-        var dec = decipher.update(text, 'hex', 'utf8');
-        dec += decipher.final('utf8');
-        return dec;
-    }
-
-    function checkSession(id, key) {
-        console.log("CHECKSESSION ID: " + id);
-        console.log("CHECKSESSION KEY: " + sessionStore[id].text);
-        console.log("DECRYPT: " + decrypt(key));
-        if (decrypt(key) === sessionStore[id].text) {
-            console.log("AUTH: Keys Matched");
-            return true;
-        } else {
-            console.log("AUTH ERROR: Keys Did Not Match");
-            return false;
-        }
-    }
 
     app.post('/timesheet', urlencodedParser, function (req, res) {
         console.log("Req: " + req.body.token);
@@ -73,7 +23,7 @@ var appRouter = function (app) {
                     {
                         "text": "Pick your engagement and time worked against it mortal!",
                         "fallback": "My magic is failing today...",
-                        "callback_id": "engagement_selected",
+                        "callback_id": req.body.text,
                         "color": "#3AA3E3",
                         "attachment_type": "default",
                         "actions": [
@@ -82,80 +32,12 @@ var appRouter = function (app) {
                                 "text": "Choose your engagement!",
                                 "type": "select",
                                 "confirm": {
-                                    "title": "Are you sure?",
-                                    "text": "",
+                                    "title": "Timesheet Confirmation",
+                                    "text": "Are you sure you want to submit a timesheet against this engagement for " + req.body.text + " hours?",
                                     "ok_text": "Yes",
                                     "dismiss_text": "No"
                                 },
                                 "data_source": "external",
-                            },
-                            {
-                                "name": "hours",
-                                "text": "Hours",
-                                "type": "select",
-                                "options": [
-                                    {
-                                        "text": "1 Hour",
-                                        "value": 1
-                                    },
-                                    {
-                                        "text": "2 Hours",
-                                        "value": 2
-                                    },
-                                    {
-                                        "text": "3 Hours",
-                                        "value": 3
-                                    },
-                                    {
-                                        "text": "4 Hours",
-                                        "value": 4
-                                    },
-                                    {
-                                        "text": "5 Hours",
-                                        "value": 5
-                                    },
-                                    {
-                                        "text": "6 Hours",
-                                        "value": 6
-                                    },
-                                    {
-                                        "text": "7 Hours",
-                                        "value": 7
-                                    },
-                                    {
-                                        "text": "8 Hours",
-                                        "value": 8
-                                    }
-                                ]
-                            },
-                            {
-                                "name": "minutes",
-                                "text": "Minutes",
-                                "type": "select",
-                                "confirm": {
-                                    "title": "Are you sure?",
-                                    "text": "",
-                                    "ok_text": "Yes",
-                                    "dismiss_text": "No"
-                                },
-                                "options": [
-                                    {
-                                        "text": "0 Minutes",
-                                        "value": 0
-                                    },
-                                    {
-                                        "text": "15 Minutes",
-                                        "value": 15
-                                    },
-                                    {
-                                        "text": "30 Minutes",
-                                        "value": 30
-                                    },
-                                    {
-                                        "text": "45 Minutes",
-                                        "value": 45
-                                    }
-                                ]
                             }
                         ]
                     }
@@ -174,31 +56,29 @@ var appRouter = function (app) {
         console.log("Action JSON: " + JSON.stringify(actionJSON));
 
         if (actionJSON.token === apiToken) {
-            if (actionJSON.actions[0].name === 'engagement_list') {
-                var action = actionJSON.actions[0].name;
+            var user_id = actionJSON.user.id;
+            var action = actionJSON.actions[0].name;
+            var callback_id = parseFloat(actionJSON.actions[0].callback_id);
+            if (action === 'engagement_list') {
                 if (actionJSON.actions[0].type === "select") {
-                    var selected_value = actionJSON.actions[0].selected_options[0].value;
-                }
-                var user_id = actionJSON.user.id;
-                var user_name = actionJSON.user.name;
-                var callback = actionJSON.callback_id;
+                    var engagement = actionJSON.actions[0].selected_options[0].value;
+                    console.log('Action: ' + action);
+                    console.log('User ID: ' + user_id);
+                    console.log('Engagement: ' + engagement);
+                    console.log('Callback ID: ' + callback_id);
 
-                console.log('Action: ' + action);
-                console.log('User ID: ' + user_id);
-                console.log('User Name: ' + user_name);
-                console.log('Selected Value: ' + selected_value);
-                console.log('Callback: ' + callback);
+                    var snJSON = {
+                        'engagement': engagement,
+                        'user_id': user_id,
+                        'time_worked': callback_id
+                    };
+                }
             }
-            var snJSON = {
-                'selected': selected_value,
-                'user_id': user_id,
-                'user_name': user_name
-            };
 
             request({
                 baseUrl: instanceURL,
                 method: 'POST',
-                uri: apiURI + '/' + callback,
+                uri: apiURI + '/engagement_selected',
                 json: true,
                 body: snJSON,
                 headers: {
