98d70ce337022f196c4762a25f9b760011466871
Eso-vendition <nic.mann@esolutionsone.com>
2017-09-06 14:39:23 -0500

    added timesheet check

--- routes/routes.js
+++ routes/routes.js
@@ -154,6 +154,7 @@ var appRouter = function (app) {
             if(callback_id === 'start_work') {
                 for (var key in messageStore) {
                     if (messageStore.hasOwnProperty(key)) {
+                        console.log("Message Store Key " + messageStore[key] + " and user " + messageStore[key].user);
                         if (key.user === 'user_id') {
                             console.log("Found Match in Message Store");
                             messageStore[actionJSON.message_ts] = messageStore[key];
